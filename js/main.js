2ba72c3afdb60670f668cbed2d7d379e1a563ed6
RuslanBuzadj <70750684+RuslanBuzadj@users.noreply.github.com>
2020-10-27 13:46:25 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -228,12 +228,12 @@ function testWebP(callback) {
   // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  var uluru = {lat: 55.72937568841019, lng: 37.625985145568855};
+  var uluru = {lat: 43.237590, lng: 76.951657};
   // The map, centered at Uluru
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 10, center: uluru});
+      document.getElementById('map'), {zoom: 14, center: uluru});
   // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
+  // var marker = new google.maps.Marker({position: uluru, map: map});
 };
 // /Webp ====================================================================
 
@@ -346,19 +346,19 @@ $(function(){
      
 });
 
-// var wow = new WOW(
-//   {
-//     boxClass:     'wow',      // animated element css class (default is wow)
-//     animateClass: 'animated', // animation css class (default is animated)
-//     offset:       0,          // distance to the element when triggering the animation (default is 0)
-//     mobile:       true,       // trigger animations on mobile devices (default is true)
-//     live:         true,       // act on asynchronously loaded content (default is true)
-//     callback:     function(box) {
-//       // the callback is fired every time an animation is started
-//       // the argument that is passed in is the DOM node being animated
-//     },
-//     scrollContainer: null,    // optional scroll container selector, otherwise use window,
-//     resetAnimation: true,     // reset animation on end (default is true)
-//   }
-// );
-// wow.init();
+var wow = new WOW(
+  {
+    boxClass:     'wow',      // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset:       0,          // distance to the element when triggering the animation (default is 0)
+    mobile:       true,       // trigger animations on mobile devices (default is true)
+    live:         true,       // act on asynchronously loaded content (default is true)
+    callback:     function(box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null,    // optional scroll container selector, otherwise use window,
+    resetAnimation: true,     // reset animation on end (default is true)
+  }
+);
+wow.init();
